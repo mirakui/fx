841aa1cf663129b8003641f4a3bce3da90f65aba
成田 一生 <mimitako@gmail.com>
2009-08-22 21:06:53 +0900

    method_adderをmethod_addableに

---
+++ lib/method_addable.rb
@@ -0,0 +1,31 @@
+module Gena
+  class MethodAddable
+
+    def self.add(noun, methods_hash)
+      methods_hash.each_pair do |verb, prc|
+        method_name = "__#{verb}_#{noun}"
+        puts method_name
+        define_method(method_name, prc)
+
+        unless method_defined?(verb)
+          puts "method not defined '#{verb}'"
+          define_method(verb, proc {|*syms|
+            if syms.count >= 2
+              syms.each do |sym|
+                res = __send__(verb, sym)
+                return res if res
+              end
+              return nil
+            elsif syms.count == 1
+              sym = syms.first
+              return __send__("__#{verb}_#{sym}")
+            else
+              return nil
+            end
+          })
+        end
+      end
+    end
+
+  end
+end
---
+++ spec/method_addable_spec.rb
@@ -0,0 +1,77 @@
+# vim:fileencoding=utf-8
+$: << File.join(File.dirname(__FILE__), '..', 'lib')
+require 'method_addable'
+
+class Fruit < Gena::MethodAddable
+
+  def initialize(value)
+    @value = value
+  end
+
+  def value
+    @value
+  end
+end
+
+FruitSpecArgsSample = [
+  :apple ,{
+    :color => proc {
+      "red"
+    },
+    :price => proc {
+      value
+    }
+  }
+]
+
+describe "MethodAddableを継承したclass Fruitを定義する場合" do
+
+  it "メソッドcolor(:apple),price(:apple)を追加できること" do
+    Fruit.add(*FruitSpecArgsSample)
+    Fruit.method_defined?("__color_apple").should be_true
+    Fruit.method_defined?("__price_apple").should be_true
+  end
+
+  it "メソッドcolorが追加されていること" do
+    Fruit.method_defined?("color").should be_true
+  end
+
+  it "メソッドpriceが追加されていること" do
+    Fruit.method_defined?("price").should be_true
+  end
+
+end
+
+describe "Fruitのインスタンスを作成したとき" do
+
+  before do
+    Fruit.add(*FruitSpecArgsSample)
+    Fruit.add(:nil_fruit, :color => proc { nil })
+    @fruit = Fruit.new(100)
+  end
+
+  it "メソッドcolor(:apple)が'red'を返すこと" do
+    @fruit.should_not be_nil
+    @fruit.color(:apple).should == 'red'
+  end
+
+  it "メソッドprice(:apple)がFruitのメンバ関数を参照できること" do
+    @fruit.price(:apple).should == 100
+  end
+
+  it "メソッドcolor(:nil_fruit)が追加できること" do
+    Fruit.method_defined?("__color_nil_fruit").should be_true
+    @fruit.color(:nil_fruit).should be_nil
+  end
+
+  it "nounを複数指定したメソッドがちゃんと動くこと" do
+    Fruit.method_defined?("__color_nil_fruit").should be_true
+    Fruit.method_defined?("color").should be_true
+    @fruit.color(:nil_fruit, :apple).should == "red"
+  end
+
+  # after do
+  # end
+end
+
+
